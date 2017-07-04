53fd6caefdaefdae6aaf3c8990d8f7280f2b45ef
Israel Hernández <thekronyck@gmail.com>
2017-07-03 22:31:10 -0600

    Add files via upload
    Arreglar lo del index, y integrar la vista de la tabla

---
+++ AngularJS_PHP/view/Table.Personaje.php
@@ -0,0 +1,17 @@
+<?php
+/*
+    require_once "../model/Class.Personaje.php";
+    $pers = Personaje::findAll();
+    if(count($pers) > 0 ):
+        foreach($pers as $item):
+            $table = '<tr>';
+            $table .= '<td>'.$item['codigo'].'</td>';
+            $table .= '<td>'.$item['nombre'].'</td>';
+            $table .= '<td>'.$item['descripcion'].'</td>';
+            $table .= '</tr>';
+            echo $table;
+        endforeach;
+    else :
+        echo "<tr><td><center>No hay datoos que mostrar</center></td></tr>";
+    endif;*/
+?>
